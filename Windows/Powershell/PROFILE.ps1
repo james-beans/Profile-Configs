8822f8110e02b2b3d82053bfa0ed59c69a6d6093
obvbeans <112166972+james-beans@users.noreply.github.com>
2024-11-19 23:50:30 +0000

    Update PROFILE.ps1

--- Windows/Powershell/PROFILE.ps1
+++ Windows/Powershell/PROFILE.ps1
@@ -1,13 +1,19 @@
 # Initialize Oh My Posh with the specified theme
-oh-my-posh init pwsh --config 'C:\Users\USERNAME_HERE\AppData\Local\Programs\oh-my-posh\themes\multiverse-neon.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\James\AppData\Local\Programs\oh-my-posh\themes\multiverse-neon.omp.json' | Invoke-Expression
 
 # ALIAS' LIST:
 
 # `make` = `mingw32-make`
-Set-Alias make mingw32-make
+#
+# if function not working use:
+# Set-Alias make mingw32-make
+# instead.
+function make {
+    mingw32-make
+}
 
-# `i` = `ii .`
-function i {
+# `files` = `ii .`
+function files {
     ii .
 }
 
@@ -16,15 +22,51 @@ function profile {
     code $PROFILE
 }
 
-# `cir` = `code .`
-function cir {
+# `codehere` = `code .`
+function codehere {
     code .
 }
 
-# `i` = `ii .`
-function cc {
+# `compilec` = `gcc main.cpp -o program`
+function compilec {
     gcc main.cpp -o program
 }
+
+# `compilecpp` = `g++ main.cpp -o program`
+function compilecpp {
+    g++ main.cpp -o program
+}
+
+# `gitcode` = `cd <PATH>`
+function gitcode {
+    cd <EDIT_THIS_PATH>
+}
+
+# `codedir` = `cd <PATH>`
+function codedir {
+    cd <EDIT_THIS_PATH>
+}
+
+# CUSTOM COMMANDS
+
+# `cfghelp` = This message.
+function cfghelp {
+    Write-Host "====================" -ForegroundColor Yellow
+    Write-Host "PowerShell Custom $-PROFILE Commands Help" -ForegroundColor Cyan
+    Write-Host "====================" -ForegroundColor Yellow
+    Write-Host "Available Custom Commands:"
+    Write-Host "  cfghelp   - Show this help message" -ForegroundColor Green
+    Write-Host "  profile   - Edits the $-PROFILE file." -ForegroundColor Green
+    Write-Host "  compilec  - Runs `gcc main.cpp -o program` " -ForegroundColor Green
+    Write-Host "  compilecpp - Runs `g++ main.cpp -o program` " -ForegroundColor Green
+    Write-Host "  make - Runs `mingw32-make` " -ForegroundColor Green
+    Write-Host "  codehere  - Opens VSCode in the current dir" -ForegroundColor Green
+    Write-Host "  gitcode  - Opens the github dir inside your code folder." -ForegroundColor Green
+    Write-Host "  files  - Opens the File Explorer in the current dir." -ForegroundColor Green
+    Write-Host "  codedir  - Opens the folder where your code is." -ForegroundColor Green
+    Write-Host "====================" -ForegroundColor Yellow
+}
+
 
 # Clear the terminal screen on startup
 Clear-Host
